221f1f1224acdb4b6389bfaa3bfa6f52bed3dd29
rekysenjaya <rekysenjaya@gmail.com>
2024-03-03 10:34:36 +0700

    add moment

--- src/components/ListChat.tsx
+++ src/components/ListChat.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import {
-  View,
   Text,
   FlatList,
   Dimensions,
   ViewToken,
   StyleSheet
 } from "react-native";
+import moment from "moment";
 import { useDispatch } from "react-redux";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { useSharedValue } from "react-native-reanimated";
 
 import { ChatItem } from "../types/TestTypes";
 
@@ -18,45 +18,11 @@ import { useChatData } from "../hooks";
 import { readChat } from "../store/user/actions";
 
 import InputChat from "./InputChat";
+import ListItem from "./ListItem";
 
-const { width, height } = Dimensions.get('screen')
+const { height } = Dimensions.get('screen')
 
-const ListItem = ({ item, index, viewbleItems, to, me }: { item: ChatItem, index: number, viewbleItems: Animated.SharedValue<ViewToken[]>, to: string, me: string }) => {
-  const listChatData = useChatData()
-  const toLast = (listChatData || [])?.[index + 1]
-  const rStyle = useAnimatedStyle(() => {
-    const isVisible = Boolean((viewbleItems?.value || []).filter((item) => item.isViewable).find((viewbleItem) => viewbleItem.item.id == item.id))
-
-    return {
-      opacity: withTiming(isVisible ? 1 : 0),
-      transform: [{
-        scale: withTiming(isVisible ? 1 : 0.6),
-      }]
-    }
-  }, [])
-
-  let roundChat = <View style={[styles.listItemRound, toLast?.to != to && { backgroundColor: 'transparent' }]} />
-  let marginBottom = 20
-
-  if (item?.to != to && toLast?.to != to) {
-    marginBottom = 10
-  }
-  if (item?.me == me && toLast?.me == me) {
-    marginBottom = 10
-  }
-  if (!toLast?.to) {
-    roundChat = <View style={styles.listItemRound} />
-  }
-
-  return <Animated.View style={[{ marginBottom }, styles.listItem, rStyle]}>
-    <View style={[styles.listItemFlex, { justifyContent: item?.to == to ? 'flex-end' : 'flex-start' }]}>
-      {!(item?.to == to) && roundChat}
-      <View style={[styles.listItemCard, { backgroundColor: item?.to == to ? '#884FE5' : '#F5F6FB' }]}>
-        <Text style={{ maxWidth: (width - 108) * .9, fontWeight: '500', fontSize: 14, color: item?.to == to ? '#fff' : '#1B1B1B', textAlign: item?.to == to ? 'right' : 'left' }}>{item?.chat}</Text>
-      </View>
-    </View>
-  </Animated.View>
-}
+const ListHeaderChat = () => <Text style={styles.listHeaderChatText}>{moment().format('MMM DD, H:mm A')}</Text>
 
 const ListChat = ({ to, me }: { to: string, me: string }) => {
   const dispatch: any = useDispatch();
@@ -93,6 +59,7 @@ const ListChat = ({ to, me }: { to: string, me: string }) => {
       onViewableItemsChanged={({ viewableItems: vItems }) => {
         viewableItems.value = vItems
       }}
+      ListHeaderComponent={<ListHeaderChat />}
       data={listChatData}
       renderItem={({ item, index }: { item: ChatItem, index: number }) => <ListItem index={index} item={item} viewbleItems={viewableItems} to={to} me={me} />}
     />
@@ -109,23 +76,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'baseline',
   },
-  listItemFlex: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    width,
-    paddingHorizontal: 25
-  },
-  listItemRound: {
-    height: 32,
-    width: 32,
-    borderRadius: 32,
-    backgroundColor: '#FC9393',
-    marginRight: 5
-  },
-  listItemCard: {
-    borderRadius: 16,
-    padding: 8,
-    paddingHorizontal: 12
+  listHeaderChatText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 13,
+    fontWeight: '700',
+    marginBottom: 25
   }
 });
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Dimensions, StyleSheet, Text, View, ViewToken } from "react-native";
+import Animated, { useAnimatedStyle, withTiming } from "react-native-reanimated";
+
+import { useChatData } from "../hooks"
+
+import { ChatItem } from "../types/TestTypes";
+
+const { width } = Dimensions.get('screen')
+
+const ListItem = ({ item, index, viewbleItems, to, me }: { item: ChatItem, index: number, viewbleItems: Animated.SharedValue<ViewToken[]>, to: string, me: string }) => {
+  const listChatData = useChatData()
+  const toLast = (listChatData || [])?.[index + 1]
+  const rStyle = useAnimatedStyle(() => {
+    const isVisible = Boolean((viewbleItems?.value || []).filter((item) => item.isViewable).find((viewbleItem) => viewbleItem.item.id == item.id))
+
+    return {
+      opacity: withTiming(isVisible ? 1 : 0),
+      transform: [{
+        scale: withTiming(isVisible ? 1 : 0.6),
+      }]
+    }
+  }, [])
+
+  let roundChat = <View style={[styles.listItemRound, toLast?.to != to && { backgroundColor: 'transparent' }]} />
+  let marginBottom = 20
+
+  if (item?.to != to && toLast?.to != to) {
+    marginBottom = 10
+  }
+  if (item?.me == me && toLast?.me == me) {
+    marginBottom = 10
+  }
+  if (!toLast?.to) {
+    roundChat = <View style={styles.listItemRound} />
+  }
+
+  return <Animated.View style={[{ marginBottom }, styles.listItem, rStyle]}>
+    <View style={[styles.listItemFlex, { justifyContent: item?.to == to ? 'flex-end' : 'flex-start' }]}>
+      {!(item?.to == to) && roundChat}
+      <View style={[styles.listItemCard, { backgroundColor: item?.to == to ? '#884FE5' : '#F5F6FB' }]}>
+        <Text style={{ maxWidth: (width - 108) * .9, fontWeight: '500', fontSize: 14, color: item?.to == to ? '#fff' : '#1B1B1B', textAlign: item?.to == to ? 'right' : 'left' }}>{item?.chat}</Text>
+      </View>
+    </View>
+  </Animated.View>
+}
+
+export default ListItem;
+
+
+const styles = StyleSheet.create({
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+  },
+  listItemFlex: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    width,
+    paddingHorizontal: 25
+  },
+  listItemRound: {
+    height: 32,
+    width: 32,
+    borderRadius: 32,
+    backgroundColor: '#FC9393',
+    marginRight: 5
+  },
+  listItemCard: {
+    borderRadius: 16,
+    padding: 8,
+    paddingHorizontal: 12
+  },
+  listHeaderChatText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 13,
+    fontWeight: '700',
+    marginBottom: 25
+  }
+});
